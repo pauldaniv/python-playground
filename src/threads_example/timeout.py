46b76455d856a71fb24285d553b8fef57e4b8c0e
Pavlo <paulsegfault@gmail.com>
2023-11-11 20:36:31 +0200

    Add Logging and threads examples

---
+++ src/threads_example/timeout.py
@@ -0,0 +1,38 @@
+import signal
+
+
+def timeout(seconds, error_message="Function call timed out"):
+    def decorator(func):
+        def handler(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapped(*args, **kwargs):
+            # Set the signal handler for SIGALRM
+            signal.signal(signal.SIGALRM, handler)
+            # Set the alarm to trigger after `seconds` seconds
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                # Cancel the alarm
+                signal.alarm(0)
+            return result
+
+        return wrapped
+
+    return decorator
+
+# Example usage:
+
+@timeout(5)  # Set a timeout of 5 seconds
+def long_running_function():
+    import time
+    time.sleep(4)  # Simulate a function taking longer than the timeout
+
+
+if __name__ == '__main__':
+
+    try:
+        long_running_function()
+    except TimeoutError as e:
+        print(f"Caught TimeoutError: {e}")
